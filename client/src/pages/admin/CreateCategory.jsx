56eeabe4d88f6c8f73d4e32323a8443fb8b9fb4e
Robiul Islam Sagar <94870525+robiulsagor@users.noreply.github.com>
2023-12-09 13:22:43 +0600

    Frontend: Delete category function done

--- client/src/pages/admin/CreateCategory.jsx
+++ client/src/pages/admin/CreateCategory.jsx
@@ -4,10 +4,12 @@ import AdminMenu from '../../components/AdminMenu'
 import CategoryForm from '../../components/form/CategoryForm'
 import toast from 'react-hot-toast'
 import axios from 'axios'
+import Loading from '../../components/Loading'
 
 const CreateCategory = () => {
     const [categories, setCategories] = useState([])
     const [value, setValue] = useState("")
+    const [selected, setSelected] = useState(null)
     const [loading, setLoading] = useState(false)
 
     const getAllCategories = async () => {
@@ -58,6 +60,31 @@ const CreateCategory = () => {
         }
     }
 
+    const deleteCategory = async () => {
+        setLoading(true)
+        try {
+            await axios.delete(`/api/category/delete-category/${selected._id}`, {
+                signal: AbortSignal.timeout(5000) //Aborts request after 5 seconds
+            })
+            toast.success(`Category has been deleted!`)
+            getAllCategories()
+            setSelected(null)
+            setLoading(false)
+            // hide modal
+            $("#deleteModal").modal("hide");
+        } catch (error) {
+            console.log(error);
+            toast.error(error?.response?.data?.message || error?.message == "canceled" && "Network Error" || "Error ")
+            setLoading(false)
+        }
+    }
+
+    // remove from state
+    const removeSelectedCategory = () => {
+        setSelected(null)
+    }
+
+
     return (
         <Layout title={"Create Category - eCommerce App"}>
             <div className="container-fluid">
@@ -70,6 +97,7 @@ const CreateCategory = () => {
                             <h2 className='mb-3'>Create Category</h2>
                             <CategoryForm handler={addCategory} value={value} setValue={setValue} loading={loading} setLoading={setLoading} />
 
+                            {/* display categories */}
                             <div className='mt-4'>
                                 <h2>Category List</h2>
                                 {categories?.length > 0 ? (
@@ -90,7 +118,7 @@ const CreateCategory = () => {
                                                     <td>{cat.name} </td>
                                                     <td>
                                                         <button type="button" className="btn btn-primary me-2" >Edit</button>
-                                                        <button type="button" className="btn btn-danger">Delete</button>
+                                                        <button type="button" className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal" onClick={(() => setSelected(cat))}>Delete</button>
                                                     </td>
                                                 </tr>
                                             ))}
@@ -101,8 +129,30 @@ const CreateCategory = () => {
                                     : <h3 className='text-center text-danger'>No data found!    </h3>}
                             </div>
                         </div>
-
                     </div>
+
+                    {/* <!-- Delete Modal --> */}
+                    <div className="modal fade" id="deleteModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div className="modal-dialog">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h1 className="modal-title fs-5" id="exampleModalLabel">Delete Category</h1>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div className="modal-body">
+                                    <p>Are you sure to delete this category- <i> <b> {selected?.name}</b> </i>? </p>
+                                </div>
+                                <div className="modal-footer">
+                                    {loading ? <Loading /> : (
+                                        <>
+                                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={removeSelectedCategory}>Cancel</button>
+                                            <button type="button" className="btn btn-danger" onClick={deleteCategory}>Delete</button>
+                                        </>)}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </Layout>
