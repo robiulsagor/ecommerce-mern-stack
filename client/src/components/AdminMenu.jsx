5d2727f2eae385b205aee4685a839548f9df89d8
Robiul Islam Sagar <94870525+robiulsagor@users.noreply.github.com>
2023-12-09 13:22:58 +0600

    Type minor changes

--- client/src/components/AdminMenu.jsx
+++ client/src/components/AdminMenu.jsx
@@ -14,6 +14,7 @@ const AdminMenu = () => {
                 <NavLink to="/admin/create-category" className="list-group-item list-group-item-action">Create Category</NavLink>
                 <NavLink to="/admin/create-product" className="list-group-item list-group-item-action">Create Product</NavLink>
                 <NavLink to="/admin/users" className="list-group-item list-group-item-action">Users</NavLink>
+                <NavLink to="/admin/secret" className="list-group-item list-group-item-action">Secret Questions</NavLink>
             </div>
         </>
     )
